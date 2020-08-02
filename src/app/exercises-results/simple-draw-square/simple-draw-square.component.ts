d43a45a559f5ee95fd6ee5b5800f25c53b307ba4
lingar <yimprogramming@gmail.com>
2020-08-02 14:16:10 +0300

    Exercise done

--- src/app/exercises-results/simple-draw-square/simple-draw-square.component.ts
+++ src/app/exercises-results/simple-draw-square/simple-draw-square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-simple-draw-square',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimpleDrawSquareComponent implements OnInit {
   x1 = "Initial value ";
+  square: ElementRef;
+  width = 50;
+  height = 50;
+  frameColor = "black";
+  fillColor = "white";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onChange(event){
+    console.log("Event = ", event);
   }
 
 }
