5ac87dc84015e109e49fa4553a9273e4c545568f
lingar <yimprogramming@gmail.com>
2020-08-06 22:25:00 +0300

    Task3 is working

--- src/app/to-delete/to-delete.component.ts
+++ src/app/to-delete/to-delete.component.ts
@@ -7,34 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDeleteComponent implements OnInit {
 
+  likes = 100;
+  dislikes = 25;
+  liked = false;
+  disliked = false;
   constructor() { }
 
   ngOnInit(): void {
-    let A = [-6, -91, 1011, -100, 84, -22, 0, 1, 473];
-    console.log(this.solution(A));
+
   }
 
-  //find odo vs pairs. if it's equal it's good.
-  solution(A) {
-    let odds = 0;
-    let evens = 0;
-    for (let i=0; i< A.length; i++){
-      if (A[i] % 2 === 0){
-        evens ++;
-      }
-      else{
-        odds++;
+  like(){
+    //switch liked state
+    this.liked = !this.liked;
+    //check  liked state
+    if (this.liked){
+      //increase
+      this.likes++;
+      //check if dislike - and if does - cancel - call to dislike() TODO
+      if(this.disliked){
+        this.dislike();
       }
     }
-    if (evens === odds){
-      return true;
+    //if liked cancelled
+    else{
+      //decrease
+      this.likes--;
     }
-    return false;
   }
 
-  reducer(acc, curr){
-
+  dislike(){
+    //switch disliked state
+    this.disliked = !this.disliked;
+    //check  disliked state
+    if (this.disliked){
+      //increase
+      this.dislikes++;
+      //check if liked - and if does - cancel - call to like()
+      if(this.liked){
+        this.like();
+      }
+    }
+    //if disliked cancelled
+    else{
+      //decrease
+      this.dislikes--;
+    }
   }
+
+
+
 
 
 }
