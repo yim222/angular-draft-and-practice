d25f605dc77dbaa27b1e6dfaf08bae85c90bb4af
lingar <yimprogramming@gmail.com>
2020-08-06 21:39:33 +0300

    Task1

--- src/app/to-delete/to-delete.component.ts
+++ src/app/to-delete/to-delete.component.ts
@@ -10,69 +10,18 @@ export class ToDeleteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    /*
-    Given A = [1, 2, 3], the function should return 4.
-
-    Given A = [−1, −3], the function should return 1.
-
-    [2,1,9,4] return 3
-     */
-    let A = [1,2,3];
-    let B =  [10, 2, -1,-3];
-    let B2 =  [ -1,-3];
-
-    let C = [22,2,1,9,4];
-    let D = [1, 3, 6, 4, 1, 2];
+    let A = [-6, -91, 1011, -100, 84, -22, 0, 1, 473];
     console.log(this.solution(A));
-    console.log(this.solution(B));
-    console.log(this.solution(B2));
-
-    console.log(this.solution(C));
-    console.log(this.solution(D));
-
-
   }
-  //find the smallest positive that not occur
-  solution(A: Array<number>) {
-    //sort the array
-    A.sort(function(a, b) {
-      return a - b;
-    });
-    console.log("after sort - " , A);
-    //cut above 0
-    let newArr = A.reduce(this.reducer, []);
-    //remove duplicates
-    // var unique = a.filter( onlyUnique );
-    newArr = newArr.filter(this.onlyUnique);
-    console.log("new array = " , newArr);
-    if (newArr.length <1){
-      return 1;
-    }else{
-      for(let i = 0 ; i< newArr.length ;i++){
-        if (i+1 < newArr[i]){
-          return i+1;
-        }
-      }
-      //if all finished so the smallest not appear positive is above the arra
-      return newArr[newArr.length-1]+1;
-    }
-
-
-
-
-    //case it's empty - it's one
-    //count against counter - when the counter < the element - it's the missing. return it.
-
+  solution(A) {
+    return A.reduce(this.reducer, 0);
   }
+
   reducer(acc, curr){
-    if(curr > 0 ){
-      acc.push(curr);
+    if (curr % 4 == 0 ){
+      acc += curr;
     }
     return acc;
-  }
-
-  onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
   }
 
 
